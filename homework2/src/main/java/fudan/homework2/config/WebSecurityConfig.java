2040b06f9a0bc021eae1102ee7bee30cdef2292d
chengweili <chengweili@192.168.5.105>
2020-10-30 22:00:10 +0800

    modifed

--- homework2/src/main/java/fudan/homework2/config/WebSecurityConfig.java
+++ homework2/src/main/java/fudan/homework2/config/WebSecurityConfig.java
@@ -6,41 +6,55 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 
 @EnableWebSecurity
 @Configuration
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		http.headers().frameOptions().sameOrigin();
+		http.cors().and().csrf().disable();
 		http.authorizeRequests()
-		//static resources
-		.antMatchers("/css/**","/js/**","/img/**","/webjars/**","/lib/**").permitAll()
-		.anyRequest().authenticated()
-		.and().formLogin().permitAll().successHandler(new AuthenticationSuccessHandler() {
-			
-			public void onAuthenticationSuccess(HttpServletRequest arg0, HttpServletResponse arg1, Authentication arg2)
-					throws IOException, ServletException {
-				// TODO Auto-generated method stub
-				
-			}
-		})
-		.and().logout().permitAll();
+				// static resources
+				.antMatchers("/css/**", "/js/**", "/img/**", "/webjars/**", "/lib/**").permitAll().anyRequest()
+				.authenticated().and().formLogin().permitAll()
+				.successHandler(new SavedRequestAwareAuthenticationSuccessHandler() {
+
+					public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+							Authentication authentication) throws IOException, ServletException {
+						super.onAuthenticationSuccess(request, response, authentication);
+					}
+				}).and().logout().permitAll();
 	}
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("Admin").password("Admin").roles("Admin")
-		.and().withUser("user").password("user").roles("user");
+		auth.inMemoryAuthentication().withUser("Admin").password("Admin").roles("admin").and().withUser("user")
+				.password("user").roles("user");
 	}
 
-	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new PasswordEncoder() {
+
+			public String encode(CharSequence rawPassword) {
+				return rawPassword.toString();
+			}
+
+			public boolean matches(CharSequence rawPassword, String encodedPassword) {
+				return rawPassword.equals(encodedPassword);
+			}
+
+		};
+	}
 }
--- homework2/src/main/java/fudan/homework2/controller/CommonController.java
+++ homework2/src/main/java/fudan/homework2/controller/CommonController.java
@@ -1,13 +1,22 @@
 package fudan.homework2.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class CommonController {
 
 	@RequestMapping("/")
-	public String home() {
-		return "index";
+	public ModelAndView home() {
+		return new ModelAndView("index");
 	}
+	
+	@GetMapping(value="/{pageId}")
+	public ModelAndView defaultMapping(@PathVariable String pageId) {
+		return new ModelAndView("pageId");
+	}
+	
 }
